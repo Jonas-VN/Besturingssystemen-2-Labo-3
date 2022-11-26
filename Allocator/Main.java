ffe1b3034b3f24bd0d4b2f2d6290405cc05de54e
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2022-11-26 17:03:14 +0100

    betere tests voor singlethreaded allocator

--- Allocator/Main.java
+++ Allocator/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         /* Tests */
-        int amount1 = 999999999;
-        int amount2 = 999999999;
-        int amount3 = 999999999;
+        int amount1 = 9;
+        int amount2 = 9;
+        int amount3 = 9;
 
         Long address1 = Allocator.instance.allocate(amount1);
         System.out.println("Allocated " + amount1 + " bytes at " + address1);
@@ -14,6 +14,17 @@ public class Main {
         System.out.println("Allocated " + amount2 + " bytes at " + address2);
         Long address3 = Allocator.instance.allocate(amount3);
         System.out.println("Allocated " + amount3 + " bytes at " + address3);
+
+        System.out.println(Allocator.instance.isAccessible(address1, amount1));
+        System.out.println(Allocator.instance.isAccessible(address2, amount2));
+        System.out.println(Allocator.instance.isAccessible(address3, amount3));
+
+        address3 = Allocator.instance.reAllocate(address3, amount3);
+        System.out.println("Reallocated " + amount3 + " bytes at " + address3);
+        address2 = Allocator.instance.reAllocate(address2, amount2);
+        System.out.println("Reallocated " + amount2 + " bytes at " + address2);
+        address1 = Allocator.instance.reAllocate(address1, amount1);
+        System.out.println("Reallocated " + amount1 + " bytes at " + address1);
 
         System.out.println(Allocator.instance.isAccessible(address1, amount1));
         System.out.println(Allocator.instance.isAccessible(address2, amount2));
